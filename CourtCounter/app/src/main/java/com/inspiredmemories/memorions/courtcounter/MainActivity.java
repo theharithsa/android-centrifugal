717cf81024bf8ba17dc1174b8f9103b719f14b42
Vishruth Harithsa <harithsa@outlook.com>
2016-07-06 22:50:07 +0530

    2nd Lesson Project Submission

---
+++ CourtCounter/app/src/main/java/com/inspiredmemories/memorions/courtcounter/MainActivity.java
@@ -0,0 +1,82 @@
+package com.inspiredmemories.memorions.courtcounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*
+    Variable Initialization
+     */
+    int scoreTeamA = 0, scoreTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /*
+    Display Methods to the Team A Scores
+    */
+    public void updateScoreThree(View view){
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void updateScoreTwo(View view){
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void updateScoreFreeThrow(View view){
+        scoreTeamA++;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /*
+    Display Methods to the Team B Scores
+    */
+    public void updateScoreThreeB(View view){
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void updateScoreTwoB(View view){
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void updateScoreFreeThrowB(View view){
+        scoreTeamB++;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Reset Function
+     */
+    public void reset(View view){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        resetMain(scoreTeamA);
+    }
+
+    public void resetMain(int score) {
+        TextView scoreViewTeamA = (TextView) findViewById(R.id.team_a_score);
+        TextView scoreViewTeamB = (TextView) findViewById(R.id.team_b_score);
+        scoreViewTeamA.setText(String.valueOf(score));
+        scoreViewTeamB.setText(String.valueOf(score));
+    }
+}
--- PracticeSet/app/src/main/java/com/example/android/practiceset/MainActivity.java
+++ PracticeSet/app/src/main/java/com/example/android/practiceset/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.android.practiceset;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // PASTE CODE YOU WANT TO TEST HERE
+        int raspberryPrice = 5;
+        display1("1 box: $" + raspberryPrice);
+        raspberryPrice = 10;
+        display2("2 boxes: $" + (raspberryPrice * 2));
+        display3("3 boxes: $" + (raspberryPrice * 3));
+    }
+
+    /**
+     * Display methods that allow the text to appear on the screen. Don't worry if you don't know
+     * how these work yet. We'll be covering them in lesson 3.
+     */
+
+    public void display(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(text);
+    }
+
+    public void display(int text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(text + "");
+    }
+
+    public void display1(String text) {
+        display(text);
+    }
+
+    public void display2(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view_2);
+        t.setText(text);
+    }
+
+    public void display3(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view_3);
+        t.setText(text);
     }
 }
