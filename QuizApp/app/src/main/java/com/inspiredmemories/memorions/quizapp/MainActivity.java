22b8eb79b6ade3eb090383e092bf7b40900b7167
Vishruth Harithsa <harithsa@outlook.com>
2016-07-16 19:10:57 +0530

    New Quiz App Added

--- QuizApp/app/src/main/java/com/inspiredmemories/memorions/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/inspiredmemories/memorions/quizapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.inspiredmemories.memorions.quizapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.lisbon_radioButton:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.stockholm_radioButton:
+                if (checked)
+                    // Ninjas rule
+                    break;
+            case R.id.newyork_radioButton:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
     }
 }
